a9561fa568b313aa3bbcc1456cf22a720140db79
Truonghocdot <truonghocdot05@gmail.com>
2026-07-12 03:33:15 +0700

    improve

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -4,12 +4,22 @@ namespace App\Providers;
 
 use App\Console\Commands\OddsRetentionCommand;
 use App\Console\Commands\OddsStatsCommand;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
+        $appUrl = trim((string) config('app.url', ''));
+        if ($appUrl !== '') {
+            URL::forceRootUrl($appUrl);
+
+            if (str_starts_with($appUrl, 'https://')) {
+                URL::forceScheme('https');
+            }
+        }
+
         if ($this->app->runningInConsole()) {
             $this->commands([
                 OddsRetentionCommand::class,
