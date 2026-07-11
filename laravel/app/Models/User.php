8e451f4d1ec0a491e95ceb42de59e475cad1928e
Truonghocdot <truonghocdot05@gmail.com>
2026-07-12 03:45:22 +0700

    improve

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\HasName;
 use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable implements FilamentUser
+class User extends Authenticatable implements FilamentUser, HasName
 {
     use Notifiable;
     use SoftDeletes;
@@ -37,5 +38,16 @@ class User extends Authenticatable implements FilamentUser
     public function canAccessPanel(Panel $panel): bool
     {
         return $this->is_active && $this->role === 'super_admin';
+    }
+
+    public function getFilamentName(): string
+    {
+        $name = trim((string) ($this->full_name ?? ''));
+
+        if ($name !== '') {
+            return $name;
+        }
+
+        return trim((string) $this->email);
     }
 }
