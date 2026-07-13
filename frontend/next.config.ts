39ce100ce9bdd039ccd6054ff65619e1e302c0be
Truonghocdot <truonghocdot05@gmail.com>
2026-07-14 01:27:33 +0700

    improve

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,7 +4,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   typedRoutes: true,
   outputFileTracingRoot: path.resolve(__dirname),
-  allowedDevOrigins: ["192.168.11.114", "*.192.168.11.114"]
 };
 
 export default nextConfig;
